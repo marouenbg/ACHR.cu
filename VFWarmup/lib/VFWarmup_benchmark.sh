48a8d2c771230e0372a4aeddf7be368abd9e800e
Marouen BEN GUEBILA <marouen.benguebila@uni.lu>
2018-01-23 17:02:13 +0100

    updated auto script

--- VFWarmup/lib/VFWarmup_benchmark.sh
+++ VFWarmup/lib/VFWarmup_benchmark.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 TIMEFORMAT=%R
-echo -e "Threads,Ecoli_core,P_Putida,Ecoli_K12," > ../data/results/smallModelsTime_VFFVA_LA.csv
+echo -e "Threads,Ecoli_core,P_Putida,Ecoli_K12,Ematrix,Ematrix_coupled,Recon2" > ../results/VFWarmup.csv
 for nthreads in 2 4 8 16 32; do
 	for run in `seq 1 3`; do
 		TimeVec="$nthreads,"
-		for model in Ecoli_core P_Putida Ecoli_K12; do
+		for model in Ecoli_core P_Putida Ecoli_K12 Ematrix Ematrix_coupled Recon2; do
 			exec 3>&1 4>&2
 			foo=$( { time mpirun -np 1 --bind-to none -x OMP_NUM_THREADS=$nthreads ./../createWarmupPts ../../../veryfastFVA/data/models/$model/$model.mps 1>&3 2>&4; } 2>&1 )
 			exec 3>&- 4>&-
