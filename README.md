6e57f4dfb535dc092baca6d722faeff9c23be3e2
Marouen <marouen.benguebila@uni.lu>
2018-04-25 17:32:31 +0200

    reprise and README file

---
+++ README.md
@@ -0,0 +1,1 @@
+This repo contains the code for ACHR.cu the cuda GPU implementation of the sampling algorithm ACHR.
--- VFWarmup/lib/testWarmupPoints.m
+++ VFWarmup/lib/testWarmupPoints.m
@@ -14,29 +14,34 @@ warmup3000=csvread('P_Putida.mps3000warmup.csv');
 warmup5000=csvread('P_Putida.mps5000warmup.csv');
 %10,000
 warmup10000=csvread('P_Putida.mps10000warmup.csv');
+%100,000
+warmup100000=csvread('P_Putida.mps100000warmup.csv');
 %%
+nSteps=1000;
+nPts=10000;
+nFiles=10;
 %sampling 100,000 points 1000 steps
 %cd 3000_warmup_Matlab
-%ACHRSampler(putida, warmupPts3000, 'Putida3000M', 10, 10000, 1000, [], [], 3600*24, 1);
+%ACHRSampler(putida, warmupPts3000, 'Putida3000M', nFiles, nPts, nSteps, [], [], 3600*24, 1);
 %cd ..
 %cd 5000_warmup_Matlab
-%ACHRSampler(putida, warmupPts5000, 'Putida5000M', 10, 10000, 1000, [], [], 3600*24, 1);
+%ACHRSampler(putida, warmupPts5000, 'Putida5000M', nFiles, nPts, nSteps, [], [], 3600*24, 1);
 %cd ..
 %
 cd 2120_warmup_VF
-ACHRSampler(putida, warmup2120, 'Putida2120', 10, 10000, 1000, [], [], 3600*24, 1);
+ACHRSampler(putida, warmup2120, 'Putida2120', nFiles, nPts, nSteps, [], [], 3600*24, 1);
 cd ..
 %
 cd 3000_warmup_VF
-ACHRSampler(putida, warmup3000, 'Putida3000', 10, 10000, 1000, [], [], 3600*24, 1);
+ACHRSampler(putida, warmup3000, 'Putida3000', nFiles, nPts, nSteps, [], [], 3600*24, 1);
 cd ..
 %
 cd 5000_warmup_VF
-ACHRSampler(putida, warmup5000, 'Putida5000', 10, 10000, 1000, [], [], 3600*24, 1);
+ACHRSampler(putida, warmup5000, 'Putida5000', nFiles, nPts, nSteps, [], [], 3600*24, 1);
 cd ..
 %
 cd 10000_warmup_VF
-ACHRSampler(putida, warmup10000, 'Putida10000', 10, 10000, 1000, [], [], 3600*24, 1);
+ACHRSampler(putida, warmup10000, 'Putida10000', nFiles, nPts, nSteps, [], [], 3600*24, 1);
 cd ..
 %
 %%
@@ -44,76 +49,36 @@ cd ..
 
 %%
 %The sampling without FVA bounds
-
+[minFlux, maxFlux] = fluxVariability(putida,0);
 %%
 figure;
 subplot(2,2,1)
-hist(warmupPts(1,:))
+hist(warmup3000(1,:))
 subplot(2,2,2)
-hist(warmup10000(1,:))
+hist(warmup5000(1,:))
 subplot(2,2,3)
-hist(warmup50000(1,:))
+hist(warmup10000(1,:))
 %%
-for i=1:length(putida.rxns)
-    if any(warmup50000(:,i) < putida.lb) || any(warmup50000(:,i)>putida.ub)
-        i
-    end
+samples2120  = loadSamples('Putida2120',10,10000);
+samples3000  = loadSamples('Putida3000',10,10000);
+samples5000  = loadSamples('Putida5000',10,10000);
+samples10000 = loadSamples('Putida10000',10,10000);
+%check CBT code fro sample processing
+%correct bounds of samples
+%Solve instead of sampling 
+%%
+figure;
+x=300;
+for i=x+1:x+64
+    subplot(8,8,i-x)
+    h1=histogram(samples2120(i,:));
+    hold on;
+    h2=histogram(samples10000(i,:));
+    h1.Normalization = 'probability';
+    bWidth=abs(h1.BinLimits(1)-h1.BinLimits(2))/10;
+    h1.BinWidth = bWidth;
+    h2.Normalization = 'probability';
+    h2.BinWidth = bWidth;
+    plot([minFlux(i) maxFlux(i)],[0 0],'*r');
+    %title(putida.rxns{i});
 end
-%%
-%for i=1:length(putida.rxns)
-    if any(centerPoint < putida.lb) || any(centerPoint > putida.ub)
-        i
-    end
-%end
-
-%%
-
-for randPointID=1:5000
-    randPointID;
-            % Pick a random warmup point
-            %randPointID = ceil(nWrmup*rand);
-            randPoint = warmupPoints(:,randPointID);
-
-            % Get a direction from the center point to the warmup point
-            u = (randPoint-centerPoint);
-            u = u/norm(u);
-
-            % Figure out the distances to upper and lower bounds
-            distUb = (maxFlux - prevPoint);
-            distLb = (prevPoint - minFlux);
-
-            % Figure out if we are too close to a boundary
-            validDir = ((distUb > dTol) & (distLb > dTol));
-            %model.rxns(~validDir)
-
-            % Zero out the directions that would bring us too close to the
-            % boundary. This may cause problems.
-            %u(~validDir) = 0;
-
-            % Figure out positive and negative directions
-            posDirn = find(u(validDir) > uTol);
-            negDirn = find(u(validDir) < -uTol);
-
-            % Figure out all the possible maximum and minimum step sizes
-            maxStepTemp = distUb(validDir)./u(validDir);
-            minStepTemp = -distLb(validDir)./u(validDir);
-            maxStepVec = [maxStepTemp(posDirn);minStepTemp(negDirn)];
-            minStepVec = [minStepTemp(posDirn);maxStepTemp(negDirn)];
-
-            % Figure out the true max & min step sizes
-            maxStep = min(maxStepVec);
-            minStep = max(minStepVec);
-            %fprintf('%f\t%f\n',minStep,maxStep);
-
-            % Find new direction if we're getting too close to a constraint
-            if (abs(minStep) < maxMinTol & abs(maxStep) < maxMinTol) | (minStep > maxStep)
-%                 fprintf('Warning %f %f\n',minStep,maxStep);
-%                 save('warningData')
-%                 continue;
-            else
-               randPointID 
-            end
-            
-end
-
-
