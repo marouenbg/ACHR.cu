6f1447a44febf9d44ee84e891af722a4416cc45a
Marouen <marouen.b.guebila@gmail.com>
2018-11-24 16:17:20 +0100

    fixed releases

--- docs/conf.py
+++ docs/conf.py
@@ -38,12 +38,7 @@ release = u'1.0'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc',
-              'sphinx.ext.mathjax',
-              'sphinx.ext.viewcode',
-              'sphinx.ext.githubpages',
-              'sphinx.ext.napoleon',
-              'releases']
+extensions = ['releases']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
